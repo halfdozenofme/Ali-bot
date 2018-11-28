3da27ad3f591d8a18920f20511c2ba7d8cebf7f3
halfdozenofme <45139744+halfdozenofme@users.noreply.github.com>
2018-11-28 07:12:25 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,8 @@
-const Telegraf = require('telegraf');
-const express = require('express');
-const expressApp = express();
-const API_TOKEN = process.env.API_TOKEN || '664045614:AAEFU0lkCBn2UMom3SnCOdY2VNmDSEIR-VA';
-const PORT = process.env.PORT || 3000;
-const URL = process.env.URL || 'https://ali-super-bot.herokuapp.com/';
-const bot = new Telegraf(API_TOKEN);
-bot.telegram.setWebhook(`${URL}/bot${664045614:AAEFU0lkCBn2UMom3SnCOdY2VNmDSEIR-VA}`);
-expressApp.use(bot.webhookCallback(`/bot${664045614:AAEFU0lkCBn2UMom3SnCOdY2VNmDSEIR-VA}`));
+var Telegraf = require('telegraf');
+const TOKEN = '664045614:AAEFU0lkCBn2UMom3SnCOdY2VNmDSEIR-VA';
+
 //Create new bot
+var bot = new Telegraf(TOKEN);
 bot.start((ctx) => ctx.reply('Hello, friends!Here I am!\nPress /go to talk to God\nPress /location to know the location\nPress /place if you need the map\nPress /video and watch the cool video from me\nWanna see me? So send this message"Show me our Queen"\nAsk me to send a sticker by sending this message"Send me a sticker, babe"\nYou can also send a sticker to me\nAnd have a nice day,guys! Forever yours, your Queen, your Dream, Ali telegram bot:*'));
 bot.command('go', function(context){
     context.reply('Hahaha sure I am runnin, stupid bitch');
@@ -36,9 +31,4 @@ bot.command('video',function(replyWithVideo){
 bot.hears('Send me a sticker, babe',function (context){
     context.replyWithSticker('CAADAgADOQADfyesDlKEqOOd72VKAg');
 });
-        expressApp.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-expressApp.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+        bot.startPolling();
