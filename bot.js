5d85d3c195befdef1a47d0f34c6d74b68c1440c9
halfdozenofme <45139744+halfdozenofme@users.noreply.github.com>
2018-11-28 07:49:30 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 60000); 
 var Telegraf = require('telegraf');
 const TOKEN = '664045614:AAEFU0lkCBn2UMom3SnCOdY2VNmDSEIR-VA';
 
